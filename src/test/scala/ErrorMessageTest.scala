3f8f4237a2e98375c11498084dc389298b97dbd4
André Hodapp <andre.hodapp@t-online.de>
2021-05-17 19:19:45 +0200

    give_code now gives the coresponding code beautiful back, but I should only print relevant lines of the code and not everything. I should change give_code so that I can specify where to start and where to end

--- src/test/scala/ErrorMessageTest.scala
+++ src/test/scala/ErrorMessageTest.scala
@@ -3,17 +3,44 @@ import org.scalatest.matchers.should.Matchers.equal
 import org.scalatest.matchers.should.Matchers._
 
 class ErrorMessageTest extends  AnyFlatSpec {
-  val what_exp = "mismatched closing delimiter"
-  val codeLines = Array("fn main() {", "println(\"Hello, world!\");", "(", "} //here after is a comment")
-
   "give_line_of_code" should "work for mismatched closing delimiter" in {
+    val what_exp = "mismatched closing delimiter"
+    val codeLines = Array("fn main() {", "println(\"Hello, world!\");", "       (", "} //here after is a comment")
+
     val column = 3
     val underline = ErrorMessage.Underline_With_Char('^', RED())
     val important_row_begin = 0
     val important_row_end = 1
     val indentionBefore = 5
     val indentionAfter = 3
-    val res = ErrorMessage.give_line_of_code(what_exp, codeLines, column, Some(underline), important_row_begin, important_row_end, indentionBefore, indentionAfter)
+    val res = ErrorMessage.give_line_of_code(what_exp, codeLines, column, Some(underline), important_row_begin, important_row_end, indentionBefore, indentionAfter,CYAN())
+    println(res)
+  }
+  "give_code" should "work for mismatched closing delimiter" in {
+    val what_exp = "mismatched closing delimiter"
+    val codeLines = Array("fn main() {", "println(\"Hello, world!\");", "       (", "} //here after is a comment")
+
+    val column = 3
+    val underline = ErrorMessage.Underline_With_Char('^', RED())
+    val important_row_begin = 0
+    val important_row_end = 1
+    val indentionBefore = 2
+    val indentionAfter = 1
+    val res = ErrorMessage.give_code(what_exp, codeLines, underline, column,important_row_begin, important_row_end, indentionBefore, indentionAfter,CYAN(), BLUE())
+    println(res)
+  }
+
+  "give_code" should "work for expected type" in {
+    val what_exp = "expected type (f32 -> (f64 -> bool)) but found type of form `(_dt31 -> (_dt31 -> _dt31))"
+    val codeLines = Array("f::F32->F64->Bool", "f=\\x->\\y->+ x y", "g::F32->F32", "g::\\x->x")
+
+    val column = 1
+    val underline = ErrorMessage.Underline_With_Char('^', RED())
+    val important_row_begin = 2
+    val important_row_end = codeLines(1).length
+    val indentionBefore = 2
+    val indentionAfter = 1
+    val res = ErrorMessage.give_code(what_exp, codeLines, underline, column,important_row_begin, important_row_end, indentionBefore, indentionAfter,CYAN(), BLUE())
     println(res)
   }
 }
